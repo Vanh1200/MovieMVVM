f63cd252000b00e6cf865183ce2ff0676335ff5c
Vanh1200 <axitpicric@gmail.com>
2020-07-20 05:45:05 +0700

    done

--- app/src/main/java/com/vanh1200/moviemvvm/base/BaseExt.kt
+++ app/src/main/java/com/vanh1200/moviemvvm/base/BaseExt.kt
@@ -1,3 +1,9 @@
 package com.vanh1200.moviemvvm.base
 
 import android.app.Activity
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+fun ImageView.setImage(url: String) {
+    Glide.with(this).load(url).into(this)
+}
